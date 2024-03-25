54f7cd75b828872f3ebabebda92cfc5ae4a3f4b5
Dale Beniga <dalecd9@gmail.com>
2024-03-25 23:42:30 +1100

    add ability to select season for stat leaders

--- nba-dashboard/src/api/nba-api/season-service.js
+++ nba-dashboard/src/api/nba-api/season-service.js
@@ -1,37 +1,46 @@
 import ApiService from "../api-service";
 
 export default class SeasonService {
-  static perGameStatLeaderCache = new Map();
+  static regStatLeaderCache = new Map();
+  static regCurrentSeason = "2023-24";
 
-  static async getPerGameStatLeaders(stat, perGame = false) {
-    if (this.perGameStatLeaderCache.has(stat)) {
-      if (perGame) {
-        return this.perGameStatLeaderCache.get(stat).perGame;
-      }
-      return this.perGameStatLeaderCache.get(stat);
+  static async getRegStatLeaders(stat, season) {
+    if (season && season !== this.regCurrentSeason) {
+      this.regCurrentSeason = season;
+      this.regStatLeaderCache.clear();
+    }
+    if (this.regStatLeaderCache.has(stat)) {
+      return this.regStatLeaderCache.get(stat);
     }
 
     try {
-      const response = await ApiService.get(`stat-leaders/${stat}`);
-      this.perGameStatLeaderCache.set(stat, response.data);
+      const response = await ApiService.get(`stat-leaders/${stat}`, {
+        params: { season: this.regCurrentSeason },
+      });
+      this.regStatLeaderCache.set(stat, response.data);
       return response.data;
     } catch (error) {
       console.error("Error fetching stat leaders", error);
     }
   }
 
-  static totalStatLeaderCache = new Map();
+  static playoffsStatLeaderCache = new Map();
+  static playoffsCurrentSeason = "2023-24";
+  static async getPlayoffsStatLeaders(stat, season) {
+    if (season && season !== this.playoffsCurrentSeason) {
+      this.playoffsCurrentSeason = season;
+      this.playoffsStatLeaderCache.clear();
+    }
 
-  static async getTotalStatLeaders(stat) {
-    if (this.totalStatLeaderCache.has(stat)) {
-      return this.totalStatLeaderCache.get(stat);
+    if (this.playoffsStatLeaderCache.has(stat)) {
+      return this.playoffsStatLeaderCache.get(stat);
     }
 
     try {
       const response = await ApiService.get(`stat-leaders/${stat}`, {
-        params: { per_mode: "Totals" },
+        params: { season_type: "Playoffs", season: this.playoffsCurrentSeason },
       });
-      this.totalStatLeaderCache.set(stat, response.data);
+      this.playoffsStatLeaderCache.set(stat, response.data);
       return response.data;
     } catch (error) {
       console.error("Error fetching stat leaders", error);
--- nba-dashboard/src/components/StatLeadersTable.vue
+++ nba-dashboard/src/components/StatLeadersTable.vue
@@ -5,22 +5,30 @@ import getPlayerHeadshot from "../utils/getPlayerHeadshot";
 import STATS from "../utils/constants/stats";
 import { onMounted, ref } from "vue";
 import SeasonService from "../api/nba-api/season-service";
-import SelectButton from "primevue/selectbutton";
 import ToggleButton from "primevue/togglebutton";
+import NBA_SEASONS_YEARS from "../utils/constants/nba-seasons";
+import Dropdown from "primevue/dropdown";
+import ProgressSpinner from "primevue/progressspinner";
 
 const selectedStat = ref("PTS");
 const statLeaders = ref([]);
+const fetchingData = ref(false);
 onMounted(async () => {
-  statLeaders.value = await SeasonService.getPerGameStatLeaders(selectedStat.value);
+  fetchingData.value = true;
+  statLeaders.value = await SeasonService.getRegStatLeaders(selectedStat.value);
+  fetchingData.value = false;
 });
 
 async function fetchStatLeaders() {
   statLeaders.value = [];
-  if (showTotal.value) {
-    statLeaders.value = await SeasonService.getTotalStatLeaders(selectedStat.value);
+
+  fetchingData.value = true;
+  if (showPlayoffLeaders.value) {
+    statLeaders.value = await SeasonService.getPlayoffsStatLeaders(selectedStat.value, selectedSeason.value);
   } else {
-    statLeaders.value = await SeasonService.getPerGameStatLeaders(selectedStat.value);
+    statLeaders.value = await SeasonService.getRegStatLeaders(selectedStat.value, selectedSeason.value);
   }
+  fetchingData.value = false;
 }
 
 async function handleStatChange(stat) {
@@ -29,23 +37,25 @@ async function handleStatChange(stat) {
   fetchStatLeaders();
 }
 
-const showTotal = ref(false);
+const showPlayoffLeaders = ref(false);
+
+const selectedSeason = ref(NBA_SEASONS_YEARS[0]);
 </script>
 
 <template>
   <div class="table-options-container">
     <ToggleButton
-      style="width: 10rem"
-      v-model="showTotal"
+      v-model="showPlayoffLeaders"
       @change="fetchStatLeaders"
-      onLabel="Total"
-      offLabel="Per Game"
+      onLabel="Playoffs"
+      offLabel="Regular Season"
     />
+    <Dropdown style="width: 10rem" v-model="selectedSeason" :options="NBA_SEASONS_YEARS" @change="fetchStatLeaders" />
   </div>
-
   <div style="padding: 1rem"></div>
 
   <DataTable
+    v-if="statLeaders.length"
     class="stat-leader-table"
     :value="statLeaders"
     paginator
@@ -65,16 +75,22 @@ const showTotal = ref(false);
       <template #body="slotProps">
         <div class="player-col">
           <div class="headshot-container">
-            <img :src="getPlayerHeadshot(slotProps.data.PLAYER_ID)" alt="Player Headshot" />
+            <img
+              :src="getPlayerHeadshot(slotProps.data.PLAYER_ID)"
+              alt="Player Headshot"
+              onerror="if (this.src != 'default.PNG') this.src = '/player-headshots/default.PNG'"
+            />
           </div>
           <p>{{ slotProps.data.PLAYER }}</p>
         </div>
       </template>
     </column>
-    <column field="TEAM" >
-      <template #header>
-        <div style="padding: 1rem">Team</div> </template
-    ></column>
+    <column field="TEAM">
+      <template #header> <div style="padding: 1rem">Team</div> </template></column
+    >
+    <column field="GP">
+      <template #header> <div style="padding: 1rem">GP</div> </template></column
+    >
     <column
       v-for="stat in STATS"
       :field="stat"
@@ -88,6 +104,9 @@ const showTotal = ref(false);
       </template>
     </column>
   </DataTable>
+  <ProgressSpinner v-else-if="fetchingData" style="width: 50px; height: 50px; display: flex; justify-content: center" />
+
+  <p v-else>No data available</p>
 </template>
 
 <style scoped>
