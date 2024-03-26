d6db8f2a4de71ce7e8521229dbe3a3731cf5ef82
Dale Beniga <dalecd9@gmail.com>
2024-03-26 21:28:18 +1100

    update api url

--- nba-dashboard/src/api/api-service.js
+++ nba-dashboard/src/api/api-service.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-
+//"http://127.0.0.1:8000/nbaapp"
 export default class ApiService {
   static initialise() {
     this.api = axios.create({
-      baseURL: "http://127.0.0.1:8000/nbaapp",
+      baseURL: "https://nba-service-2ts5y.ondigitalocean.app/nbaapp",
     });
   }
 
