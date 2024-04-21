00e7f7ac87463a92bd9a1bbe01e45ba81f83f449
Dale Beniga <dalecd9@gmail.com>
2024-04-21 20:41:01 +1000

    Moved award winners json to static file

--- nba-service/nbaservice/settings.py
+++ nba-service/nbaservice/settings.py
@@ -35,7 +35,8 @@ CORS_ALLOW_HEADERS = "access-control-allow-origin"
 # CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://nba-dashboard-jxkcl.ondigitalocean.app"
+    "https://nba-dashboard-jxkcl.ondigitalocean.app",
+    "https://nba-archives.live"
 ]
 
 CORS_ALLOW_METHODS = [
